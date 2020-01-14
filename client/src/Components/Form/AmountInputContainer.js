0037c5c8cea0222497031c0f8708cacd3404a7ec
Michael Bauer <michael@m-bauer.org>
2020-01-14 14:45:05 +0100

    refactor: simplify AmountInputContainer.js

--- client/src/Components/Form/AmountInputContainer.js
+++ client/src/Components/Form/AmountInputContainer.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 import {toBaseUnit} from '../../erc20-decimals-conversion'
+import {Input} from 'semantic-ui-react'
 
 /*
     Tasks for this component:
@@ -8,26 +9,20 @@ import {toBaseUnit} from '../../erc20-decimals-conversion'
       has enough balance!
     - Convert user-friendly value provided through input component to raw token amount (decimals!)
     - Provide entered raw token amount to parent component through props.setAmount()
-    - Convert raw token amount to user-friendly string for rendering back in input component
  */
 const AmountInputContainer = ({web3, setAmount, decimals}) => {
-    const [localAmount, setLocalAmount] = useState('0')
-
-    useEffect(() => {
-        let amount = toBaseUnit(localAmount, decimals, web3.utils.BN)
-        console.log(`Converted ${localAmount} to ${amount.toString()}`)
-        setAmount(amount)
-    }, [localAmount, decimals])
 
     const onchange = (ev) => {
         let newValue = ev.target.value
         console.log(`New value from input: ${newValue}`)
-        setLocalAmount(ev.target.value)
+        let amount = toBaseUnit(newValue, decimals, web3.utils.BN)
+        console.log(`Converted ${newValue} to ${amount.toString()}`)
+        setAmount(amount)
     }
 
     return (
         <div>
-            <input type='number' value={localAmount} onChange={onchange}></input>
+            <Input type='number' onChange={onchange}></Input>
         </div>
     )
 }
