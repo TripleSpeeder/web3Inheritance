d68b318f3cfd52721d1d634a644cb6342e532d61
Michael Bauer <michael@m-bauer.org>
2020-01-20 09:34:12 +0100

    Improve error feedback

--- client/src/Components/StreamModal.js
+++ client/src/Components/StreamModal.js
@@ -14,13 +14,18 @@ const StreamModal = ({web3, open, handleClose}) => {
     const [streamOptions, setStreamOptions] = useState({})
     const [phase, setPhase] = useState(0)
     const [availableTokens, setAvailableTokens] = useState({})
+    const [loadingTokens, setLoadingTokens] = useState(true)
+    const [loadingSablier, setLoadingSablier] = useState(true)
+    const [loadingAccount, setLoadingAccount] = useState(true)
 
     useEffect(() => {
         const obtainTokenInstances = async() => {
+            setLoadingTokens(true)
             setAvailableTokens(await loadTokens(web3))
+            setLoadingTokens(false)
         }
         const obtainSealedSablierInstance = async () => {
-            // Get the contract instance.
+            setLoadingSablier(true)
             const SealedSablierContract = contract(SealedSablierContractData)
             SealedSablierContract.setProvider(web3.currentProvider)
             try {
@@ -30,6 +35,7 @@ const StreamModal = ({web3, open, handleClose}) => {
             }catch(error){
                 console.log(error)
             }
+            setLoadingSablier(false)
         }
         obtainTokenInstances()
         obtainSealedSablierInstance()
@@ -37,6 +43,7 @@ const StreamModal = ({web3, open, handleClose}) => {
 
     useEffect(() => {
         const obtainAccount = async () => {
+            setLoadingAccount(true)
             try {
                 // Request account access
                 await window.ethereum.enable();
@@ -46,8 +53,8 @@ const StreamModal = ({web3, open, handleClose}) => {
                 console.log("Got account")
             } catch (error) {
                 console.log('User rejected connection request')
-                handleClose()
             }
+            setLoadingAccount(false)
         }
         if(open) {
             obtainAccount()
@@ -63,23 +70,79 @@ const StreamModal = ({web3, open, handleClose}) => {
 
     let content
 
-    if (!sealedSablierContract) {
-        // return <Segment>Waiting for SealedSablier contract instance...</Segment>
+    // Got account?
+    if (account === '') {
+        if (loadingAccount) {
+            console.log("Still waiting for account...")
+            content = <Message info icon>
+                <Icon name={'spinner'} loading/>
+                <Message.Content>
+                    <Message.Header>Connect wallet</Message.Header>
+                    Please approve connection to your account to continue
+                </Message.Content>
+            </Message>
+        } else {
+            content = <Message error icon>
+                <Icon name={'exclamation'}/>
+                <Message.Content>
+                    <Message.Header>Could not connect account</Message.Header>
+                    <p>You need to approve the account connection in order to setup a stream.</p>
+                    <Button color={'black'} onClick={handleClose}>Okay</Button>
+                </Message.Content>
+            </Message>
+
+        }
+    }
+    // Got SealedSablier contract?
+    else if (!sealedSablierContract) {
+        if (loadingSablier) {
+            // still loading...
+            content = <Message info icon>
+                <Icon name={'spinner'} loading/>
+                <Message.Content>
+                    <Message.Header>Loading SealedSablier contract</Message.Header>
+                    Please wait while loading SealedSablier contract
+                </Message.Content>
+            </Message>
+        } else {
+            // loading failed :-(
+            content = <Message error icon>
+                <Icon name={'exclamation'}/>
+                <Message.Content>
+                    <Message.Header>Could not load SealedSablier contract</Message.Header>
+                    <p>
+                    Are you running on a local dev chain? Make sure to execute <em>truffle
+                    migrate</em> to deploy instances of Sablier and SealedSablier contracts.
+                    </p>
+                    <Button color={'black'} onClick={handleClose}>Okay</Button>
+                </Message.Content>
+            </Message>
+        }
         console.warn(`SealedSablier contract has not been deployed on current network`)
     }
-
-    if (account === '') {
-        console.log("Still waiting for account...")
-        content = <Message info icon header='please connect wallet'>
-            <Icon name={'info'}/>
-            Please approve connection to your account to continue
-        </Message>
-    }
+    // Got Token contract(s)?
     else if (!Object.keys(availableTokens).length) {
-        content = <Message info icon header='Loading token contracts'>
-            <Icon name={'spinner'} loading/>
-            Please wait while loading token contracts
-        </Message>
+        if (loadingTokens) {
+            // still loading...
+            content = <Message info icon>
+                <Icon name={'spinner'} loading/>
+                <Message.Content>
+                    <Message.Header>Loading token contracts</Message.Header>
+                    Please wait while loading token contracts
+                </Message.Content>
+            </Message>
+        } else {
+            // loading failed :-(
+            content = <Message error icon>
+                <Icon name={'exclamation'}/>
+                <Message.Content>
+                    <Message.Header>No token contracts available</Message.Header>
+                    <p>Are you running on a local dev chain? Make sure to execute <em>truffle
+                        migrate</em> to deploy an ERC20 Mock contract.</p>
+                    <Button color={'black'} onClick={handleClose}>Okay</Button>
+                </Message.Content>
+            </Message>
+        }
     }
     else if (phase===0) {
         content = <StreamForm
