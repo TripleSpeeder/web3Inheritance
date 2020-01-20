9f85497247d0326950d799a97fb59e0db875ae5e
Michael Bauer <michael@m-bauer.org>
2020-01-20 13:58:43 +0100

    Add test networks

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -12,14 +14,54 @@ module.exports = {
     },
     main: {
       network_id: "1",
-      host: "fullnode.dappnode",
-      port: 8545
+      provider: function() {
+        const private_key = process.env.PRIVATE_KEY
+        if (private_key === undefined) {
+          throw Error('Provide private key through .env!')
+        }
+        return new HDWalletProvider(private_key, "http://fullnode.dappnode:8545");
+      },
+    },
+    ropsten: {
+      network_id: '3',
+      provider: function() {
+        const private_key = process.env.PRIVATE_KEY
+        if (private_key === undefined) {
+          throw Error('Provide private key through .env!')
+        }
+        return new HDWalletProvider(private_key, "https://ropsten.infura.io/v3/6e94a46ee6204d4eae1285bb4c9cb639");
+      }
     },
     kovan: {
       network_id: "42",
+      provider: function() {
+        const private_key = process.env.PRIVATE_KEY
+        if (private_key === undefined) {
+          throw Error('Provide private key through .env!')
+        }
+        return new HDWalletProvider(private_key, "https://kovan.infura.io/v3/6e94a46ee6204d4eae1285bb4c9cb639");
+      }
     },
     rinkeby: {
       network_id: "4",
+      provider: function() {
+        const private_key = process.env.PRIVATE_KEY
+        if (private_key === undefined) {
+          throw Error('Provide private key through .env!')
+        }
+        return new HDWalletProvider(private_key, "https://rinkeby.infura.io/v3/6e94a46ee6204d4eae1285bb4c9cb639");
+      }
+    },
+    goerli: {
+      network_id: "5",
+      gasLimit: "8000000",
+      provider: function() {
+        const private_key = process.env.PRIVATE_KEY
+        if (private_key === undefined) {
+          throw Error('Provide private key through .env!')
+        }
+        return new HDWalletProvider(private_key, "https://goerli.infura.io/v3/6e94a46ee6204d4eae1285bb4c9cb639");
+      }
     }
   },
   compilers: {
