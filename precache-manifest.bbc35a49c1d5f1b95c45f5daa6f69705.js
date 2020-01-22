f4ef46fd0a2255e61ae798d9458a02cd7bad4383
Michael Bauer <michael@m-bauer.org>
2020-01-22 13:52:44 +0100

    Updates

---
+++ precache-manifest.bbc35a49c1d5f1b95c45f5daa6f69705.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d64910f89ac64e7b66d8225cdb5a54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9223c6b827986f131aed",
+    "url": "/static/css/main.1a8d6906.chunk.css"
+  },
+  {
+    "revision": "b306760c73615c3e095c",
+    "url": "/static/js/2.ce5f9f87.chunk.js"
+  },
+  {
+    "revision": "9223c6b827986f131aed",
+    "url": "/static/js/main.a36a6111.chunk.js"
+  },
+  {
+    "revision": "bb3db1b794bd9325906d",
+    "url": "/static/js/runtime-main.96118375.js"
+  }
+]);
